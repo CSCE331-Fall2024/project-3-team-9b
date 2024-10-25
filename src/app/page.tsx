d11a97c45e2efbf42edd686803b9ae3f708b2bed
nicktnc244 <nicktnc24@gmail.com>
2024-10-25 12:31:15 -0500

    basic working GUI of homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center">Home</div>
+    <div className="flex flex-col items-center h-screen">
+      <div className="text-6xl mt-10">
+        Welcome to Panda Express!
+      </div>
+      <div className="">
+        Produced by: Nick Truong, Anthony Huang, Carson Coen, Ethan Ghoreishi, Nathan Cornelius
+      </div>
+      <div className="text-2xl mt-24">
+        Login As:
+      </div>
+      <button className="w-1/4 bg-black text-white rounded">Customer</button>
+      <button className="w-1/4 bg-black text-white rounded">Cashier</button>
+      <button className="w-1/4 bg-black text-white rounded">Manager</button>
+        
+    </div>
   );
 }
