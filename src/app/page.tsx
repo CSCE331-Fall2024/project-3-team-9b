6e642f5f25519b7f203e9f183f302701e51c6c6f
nicktnc244 <nicktnc24@gmail.com>
2024-11-04 11:52:30 -0600

    consistent styling to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,10 @@ export default function Home() {
       <div className="text-2xl mt-24">
         Login As:
       </div>
-      <div className="flex flex-col items-center w-full gap-y-6 my-7">
-        <Link href="/customerView" className="w-1/6 py-10 bg-red-600 text-white rounded hover:scale-110 hover:duration-300 text-center">Customer</Link>
-        <Link href="/cashierView" className="w-1/6 py-10 bg-red-600 text-white rounded hover:scale-110 hover:duration-300 text-center">Cashier</Link>
-        <Link href="/managerView" className="w-1/6 py-10 bg-red-600 text-white rounded hover:scale-110 hover:duration-300 text-center">Manager</Link>
+      <div className="flex flex-col items-center w-full gap-y-6 my-7 text-2xl text-gray-800 font-bold">
+        <Link href="/customerView" className="w-1/5 py-10 bg-white rounded-lg shadow-lg  hover:scale-110 hover:duration-300 text-center">Customer</Link>
+        <Link href="/cashierView" className="w-1/5 py-10 bg-white rounded-lg shadow-lg   hover:scale-110 hover:duration-300 text-center">Cashier</Link>
+        <Link href="/managerView" className="w-1/5 py-10 bg-white rounded-lg shadow-lg  hover:scale-110 hover:duration-300 text-center">Manager</Link>
       </div>
       <div className="text-2xl mt-22">
         <Link href = "/menuBoardView">Or View Menu</Link>
