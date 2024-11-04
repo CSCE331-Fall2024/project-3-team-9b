e10fd274b9d3453fd05aa548a42fd949497de826
Nathan Cornelius <nmcorn21@tamu.edu>
2024-11-03 18:25:55 -0600

    added inventory api and verified it

---
+++ src/app/api/fetchInventory/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from 'next/server';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+  ssl: { rejectUnauthorized: false }, // Modify based on SSL requirements
+});
+
+export async function GET() {
+  try {
+    const result = await pool.query('SELECT * FROM ingredients;');
+    return NextResponse.json({ ingredients: result.rows });
+  } catch (error) {
+    console.error('Error fetching ingredients:', error); // This will give us more details about the error
+    return NextResponse.json({ error: 'Error fetching ingredients data' }, { status: 500 });
+  }
+}
+
+console.log("Database Configuration:", {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD ? '*****' : '(not set)',
+    port: process.env.DB_PORT,
+  });
+  
