fcc28e7bd6b25950df0e8e25483b1380bd2a60f7
nicktnc244 <nicktnc24@gmail.com>
2024-11-05 16:33:08 -0600

    made shopping cart gui

--- src/app/_components/shoppingCart.tsx
+++ src/app/_components/shoppingCart.tsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
+
 export default function ShoppingCart(){
+    const [cart, showCart] = useState(false);
+    const toggleCart = () => {
+        showCart(!cart);
+    };
     const currentItems: string[] = [];
+    const addItem = (item: string) => {
+        currentItems.push(item);
+    }; 
     return (
-        <button className="absolute right-2 top-2" onClick={()=> console.log("ex")}><img src = "/ShoppingCart.png"></img></button>
+        <>
+            <button className="absolute right-2 top-2 z-10" onClick={toggleCart}><img src = "/ShoppingCart.png"></img></button>
+            <div className={`${cart ? "" : "hidden"} fixed bg-gray-700 h-full w-96 right-0 bg-opacity-60 flex rounded-lg flex-col items-center justify-center`}>
+                <div className="bg-white rounded-lg h-4/5 w-3/4 flex flex-col items-center">
+                    <div className="text-gray-800 font-bold text-2xl">Your Items:</div>
+                </div>
+            </div>
+        </>
     )
 }
