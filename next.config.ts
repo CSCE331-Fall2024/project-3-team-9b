6cb677cb16828effbf834bf62f50ca0d56ec8d70
nicktnc244 <nicktnc24@gmail.com>
2024-11-27 17:34:10 -0600

    m

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export',
-  basePath: '/project-3-team-9b',
+
 };
 
 export default nextConfig;
